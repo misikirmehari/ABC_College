c9306b4d023816ff0d12988b6ed9e8a27b14d785
Misikir <misikirmehari@gmail.com>
2017-02-28 22:10:15 -0700

    Removed sections model

--- app/models/section.rb
+++
@@ -1,6 +0,0 @@
-class Section < ApplicationRecord
-  validates :course_id ,:student_id, :professor_id, presence:true
-  belongs_to :student
-  belongs_to :professor
-  belongs_to :course
-end
--- spec/factories/sections.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :section do
-    student nil
-    professor nil
-    course nil
-  end
-end
